a895b4bebc925c95cc256c2c86d89974da094add
Vitor <castanheira@discente.ufg.br>
2025-01-09 22:10:19 -0300

    add button function

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,29 +1,54 @@
 import React from "react";
-import { 
-    Text, 
+import {
+    Text,
     View,
     Image,
     TextInput,
-    TouchableOpacity
- } from "react-native"
+    TouchableOpacity,
+    Alert,
+    ActivityIndicator
+} from "react-native"
 import { style } from "./styles";
 import Logo from "../../assets/logo.png"
-import {MaterialIcons} from '@expo/vector-icons'
+import { MaterialIcons } from '@expo/vector-icons'
 import { themas } from "../../global/themes";
 
 
-export default function Login (){
-    return(
+export default function Login() {
+    const [email, setEmail] = React.useState('')
+    const [password, setPassword] = React.useState('')
+    const [loading, setLoading] = React.useState(false)
+
+    async function getLogin() {
+        try{
+            setLoading(true)
+            if(!email || !password){
+                return Alert.alert('Atenção', 'Preencha todos os campos')
+            }
+
+            setTimeout(()=>{
+                Alert.alert('Logado com sucesso')
+                setLoading(false)
+            },1500)
+
+
+
+        }catch(error){
+            Alert.alert('Erro ao fazer login')
+        }
+    }
+
+    return (
 
         <View style={style.container}>
-            
+
             <View style={style.boxTop}>
                 <Image
-                source={Logo}
-                style={style.logo}
-                resizeMode= "contain"
+                    source={Logo}
+                    style={style.logo}
+                    resizeMode="contain"
                 />
-            <Text style={style.text}>Seja bem vindo novamente!!!!</Text>
+                <Text style={style.text}>Seja bem vindo novamente!!!!</Text>
 
             </View>
 
@@ -32,34 +57,44 @@ export default function Login (){
                 <View style={style.boxInput}>
                     <TextInput
                         style={style.input}
-                     />
+                        value= {email}
+                        onChangeText={setEmail}
+                    />
                     <MaterialIcons
-                        name = 'email'
-                        size = {20}
-                        color = {themas.Colors.gray}
+                        name='email'
+                        size={20}
+                        color={themas.Colors.gray}
                     />
                 </View>
-                
+
                 <Text style={style.titleInput}>Senha</Text>
                 <View style={style.boxInput}>
                     <TextInput
+                        secureTextEntry={true}
                         style={style.input}
-                     />
+                        value={password}
+                        onChangeText={setPassword}
+                    />
                     <MaterialIcons
-                        name = 'remove-red-eye'
-                        size = {20}
-                        color = {themas.Colors.gray}
+                        name='remove-red-eye'
+                        size={20}
+                        color={themas.Colors.gray}
                     />
                 </View>
             </View>
 
             <View style={style.boxBottom}>
-                <TouchableOpacity style={style.button}>
-                    <Text style={style.textButton}>Entrar</Text>
+                <TouchableOpacity style={style.button} onPress={getLogin}>
+                    {loading ? 
+                        <ActivityIndicator color={'white'} size={'small'}/>
+                    :
+                        <Text style={style.textButton}>Entrar</Text> 
+                    }
                 </TouchableOpacity>
             </View>
-            <Text style={style.textBottom}>Não tem conta? Crie agora</Text>
-
+            <Text style={style.textBottom}>Não tem conta?
+                <Text style={{ color: themas.Colors.primary, fontWeight: 'bold' }}>Crie agora</Text>
+            </Text>
 
         </View>
 
